d56518ca7138aeaafb3a490e2ceba80f7d3951fb
Hannah Dadd <hannahdadd@hannahs-air.home>
2022-01-23 23:11:09 +0000

    firebase app

---
+++ GetItWrite/GetItWriteApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetItWriteApp.swift
+//  GetItWrite
+//
+//  Created by Hannah Dadd on 23/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct GetItWriteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
