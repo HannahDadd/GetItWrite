05c41e085ad6cb566fb0d947b9b871a14086a125
Hannah Billingsley-Dadd <hannah.billingsley-dadd01@bbc.co.uk>
2022-02-18 18:07:32 +0000

    tag view box

--- GetItWrite/Views/Login/CreateAccountView.swift
+++ GetItWrite/Views/Login/CreateAccountView.swift
@@ -13,7 +13,6 @@ struct CreateAccountView: View {
     @State private var displayName: String = ""
     @State private var bio: String = ""
     @State private var authors: [String] = []
-    @State private var author: String = ""
     @State private var errorMessage: String = ""
 
     var body: some View {
@@ -22,21 +21,7 @@ struct CreateAccountView: View {
                 Image("Lounging").resizable().aspectRatio(contentMode: .fit).padding()
                 TextField("Display name", text: $displayName).textFieldStyle(RoundedBorderTextFieldStyle())
                 TextEditor(text: $bio)
-                HStack {
-                    TextField("Choose author", text: self.$author)
-                    Button(action: {
-                        if author != "" {
-                            self.authors.append(self.author)
-                            self.author = ""
-                        }
-                    }) {
-                        Image(systemName: "plus.circle")
-                    }
-                }
-                Text("Tap tags to remove").font(.caption)
-                TagCloud(tags: self.authors, onTap: { text in
-                    self.authors = self.authors.filter { $0 != text }
-                })
+                TagBoxView(array: $authors, textLabel: "Add author", questionLabel: "Who are your favourite authors?")
                 Text(errorMessage).foregroundColor(Color.red).fixedSize(horizontal: false, vertical: true)
                 Button(action: {}) {
                     Text("SIGN UP!").bold()
@@ -47,6 +32,34 @@ struct CreateAccountView: View {
                         .overlay(RoundedRectangle(cornerRadius: 5))
                 }.accentColor(Color.clear)
             }.padding().navigationBarTitle(Text("Create Account"), displayMode: .inline)
+        }
+    }
+}
+
+struct TagBoxView: View {
+    @Binding var array: [String]
+    @State private var value: String = ""
+    let textLabel: String
+    let questionLabel: String
+
+    var body: some View {
+        VStack {
+            Text(questionLabel).font(.caption).bold().frame(maxWidth: .infinity, alignment: .leading)
+            HStack {
+                TextField(textLabel, text: self.$value)
+                Button(action: {
+                    if value != "" {
+                        self.array.append(self.value)
+                        self.value = ""
+                    }
+                }) {
+                    Image(systemName: "plus.circle")
+                }
+            }
+            Text("Tap tags to remove").font(.caption)
+            TagCloud(tags: self.array, onTap: { text in
+                self.array = self.array.filter { $0 != text }
+            })
         }
     }
 }
