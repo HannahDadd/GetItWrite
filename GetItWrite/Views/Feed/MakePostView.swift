2e7e39c222624afc7220d60425868d1c2db90663
Hannah Billingsley-Dadd <hannah.billingsley-dadd01@bbc.co.uk>
2022-04-12 22:28:33 +0100

    refactor

--- GetItWrite/Views/Feed/MakePostView.swift
+++ GetItWrite/Views/Feed/MakePostView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct MakePostView: View {
 	@EnvironmentObject var session: FirebaseSession
-
+	
 	@State private var text: String = ""
 	@State private var title: String = ""
 	@State private var synopsisSoFar: String = ""
@@ -17,37 +17,37 @@ struct MakePostView: View {
 	@State var genres: [String] = []
 	@State var typeOfWork: String = ""
 	@State private var midBook = true
-
+	
 	@Binding var showingComposeMessage: Bool
-
+	
 	var body: some View {
 		NavigationView {
-		ScrollView(.vertical) {
-			VStack(spacing: 20) {
-				TextField("Title", text: $title).textFieldStyle(RoundedBorderTextFieldStyle())
-				SingleTagSelectView(chosenTag: $typeOfWork, questionLabel: "Type of Work:", array: GlobalVariables.typeOfWork)
-				if typeOfWork == GlobalVariables.typeOfWork[2] {
-					Toggle("Is this mid book?", isOn: $midBook)
-					if midBook {
-						Text("Add a synopsis of any required information for your reader to understand the chapter").bold()
-						TextEditor(text: $synopsisSoFar)
-							.frame(height: 100, alignment: .leading)
-							.cornerRadius(6.0)
-							.border(Color.gray, width: 1)
-							.multilineTextAlignment(.leading)
+			ScrollView(.vertical) {
+				VStack(spacing: 20) {
+					TextField("Title", text: $title).textFieldStyle(RoundedBorderTextFieldStyle())
+					SingleTagSelectView(chosenTag: $typeOfWork, questionLabel: "Type of Work:", array: GlobalVariables.typeOfWork)
+					if typeOfWork == GlobalVariables.typeOfWork[2] {
+						Toggle("Is this mid book?", isOn: $midBook)
+						if midBook {
+							Text("Add a synopsis of any required information for your reader to understand the chapter").bold()
+							TextEditor(text: $synopsisSoFar)
+								.frame(height: 100, alignment: .leading)
+								.cornerRadius(6.0)
+								.border(Color.gray, width: 1)
+								.multilineTextAlignment(.leading)
+						}
 					}
+					QuestionSection(text: "Blurb", response: $blurb)
+					SelectTagView(chosenTags: $genres, questionLabel: "Genre of piece:", array: GlobalVariables.genres)
+					StretchedButton(text: "POST", action: {
+						self.session.post(title: title, text: text, synopsisSoFar: synopsisSoFar, typeOfWork: typeOfWork, blurb: blurb, genres: genres)
+						self.showingComposeMessage.toggle()
+					})
 				}
-				QuestionSection(text: "Blurb", response: $blurb)
-				SelectTagView(chosenTags: $genres, questionLabel: "Genre of piece:", array: GlobalVariables.genres)
-				StretchedButton(text: "POST", action: {
-					self.session.post(title: title, text: text, synopsisSoFar: synopsisSoFar, typeOfWork: typeOfWork, blurb: blurb, genres: genres)
-					self.showingComposeMessage.toggle()
-				})
-			}
-		}.padding().navigationBarItems(
-			trailing: Button(action: { self.showingComposeMessage.toggle() }) {
-				Text("Cancel")
-			}).navigationBarTitle(Text("New Work"), displayMode: .inline)
+			}.padding().navigationBarItems(
+				trailing: Button(action: { self.showingComposeMessage.toggle() }) {
+					Text("Cancel")
+				}).navigationBarTitle(Text("New Work"), displayMode: .inline)
 		}
 	}
 }
