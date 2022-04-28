420c77c1103818f3c69b5a46757e94aa7a6a27a6
Hannah Billingsley-Dadd <hannah.billingsley-dadd01@bbc.co.uk>
2022-04-28 18:12:08 +0100

    colours

--- GetItWrite/Extensions/GlobalVariables.swift
+++ GetItWrite/Extensions/GlobalVariables.swift
@@ -6,8 +6,12 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct GlobalVariables {
-	static var genres = ["Young Adult", "Adult", "Middle Grade", "Fantasy", "Magical Realism", "Histroical", "Romance", "Science Fiction", "Women's Fiction", "Short Stories", "Dystopian", "Mystery", "Thriller"]
+	static var genres = ["Young Adult", "Adult", "Middle Grade", "Fantasy", "Magical Realism", "Histroical", "Romance", "Science Fiction", "Memoir", "Women's Fiction", "Short Stories", "Dystopian", "Mystery", "Thriller"]
+
 	static var typeOfWork = ["Query Letter", "Short Story", "Chapter of Book", "Picture Book Text", "Synopsis"]
+
+	static var profileColours = [Color("Grey"), Color("DarkBlue"), Color("DarkestGreen"), Color("DarkGreen"), Color("DarkYellow"), Color("LightBlue"), Color("LightGreen"), Color("Yellow")]
 }
