201792a5f284479e0aa21f67f0846d92b03412a3
Hannah Billingsley-Dadd <hannah.billingsley-dadd01@bbc.co.uk>
2022-07-01 11:25:02 +0100

    style of onboarding on messages

--- GetItWrite/Views/Messages/AllChatsView.swift
+++ GetItWrite/Views/Messages/AllChatsView.swift
@@ -10,17 +10,17 @@ import SwiftUI
 struct AllChatsView: View {
 	@EnvironmentObject var session: FirebaseSession
 	@State private var result: Result<[Chat], Error>?
-
+	
 	var body: some View {
 		switch result {
 		case .success(let chats):
-			if chats.count == 0 {
-				VStack(alignment: .leading, spacing: 24) {
-					Text("You have no chats.").font(.title2)
-					Text("Select 'Swap' on the side bar 👈 to find critique partners.")
-				}
-			}
 			List {
+				if chats.count == 0 {
+					VStack(alignment: .leading, spacing: 24) {
+						Text("You have no chats.").font(.title2)
+						Text("Select 'Swap' on the side bar 👈 to find critique partners.")
+					}
+				}
 				ForEach(chats, id: \.self) { i in
 					MessagePreview(chat: i)
 				}
@@ -37,7 +37,7 @@ struct AllChatsView: View {
 			ProgressView().onAppear(perform: loadChats)
 		}
 	}
-
+	
 	private func loadChats() {
 		session.loadAllChats {
 			result = $0
