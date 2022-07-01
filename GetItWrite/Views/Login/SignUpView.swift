a379e05fbc492d0fc21db18ccc194abff2b0c1f4
Hannah Billingsley-Dadd <hannah.billingsley-dadd01@bbc.co.uk>
2022-07-01 11:20:30 +0100

    ts and cs

--- GetItWrite/Views/Login/SignUpView.swift
+++ GetItWrite/Views/Login/SignUpView.swift
@@ -9,50 +9,62 @@ import SwiftUI
 import FirebaseAuth
 
 struct SignUpView: View {
-    @Environment(\.presentationMode) var presentation
-    @EnvironmentObject var session: FirebaseSession
-
-    @State private var email: String = ""
-    @State private var password: String = ""
-    @State private var errorMessage: String = ""
-    @State private var confirmPassword: String = ""
-
-    @State var changePage = false
-
-    var body: some View {
-        VStack {
-            Image("Building").resizable().aspectRatio(contentMode: .fit).padding()
-            Text("Sign Up").font(.largeTitle).bold().frame(maxWidth: .infinity, alignment: .leading)
-            TextField("Email", text: $email).textFieldStyle(RoundedBorderTextFieldStyle())
-            TextField("Password", text: $password).textFieldStyle(RoundedBorderTextFieldStyle())
-            TextField("Confirm password", text: self.$confirmPassword).textFieldStyle(RoundedBorderTextFieldStyle())
-            StretchedButton(text: "SIGN UP", action: signUp)
-            Text(errorMessage).foregroundColor(Color.red).fixedSize(horizontal: false, vertical: true)
-            Spacer()
-            Button(action: { presentation.wrappedValue.dismiss() }) {
-                Text("Back to Login").foregroundColor(Color.lightBackground).bold()
-            }
-            NavigationLink(destination: CreateAccountView().environmentObject(session), isActive: self.$changePage) {
-                 EmptyView()
-            }
-        }.padding().navigationBarHidden(true)
-    }
-
-    func signUp() {
-        if !email.isEmpty && !password.isEmpty {
-            if password == confirmPassword {
-                session.signUp(email: email, password: password) { (result, error) in
-                    if let error = error {
-                        errorMessage = error.localizedDescription
-                    } else {
-                        changePage = true
-                    }
-                }
-            } else {
-                errorMessage = "Passwords do not match"
-            }
-        } else {
-            errorMessage = "Please provide an email and password"
-        }
-    }
+	@Environment(\.presentationMode) var presentation
+	@EnvironmentObject var session: FirebaseSession
+	
+	@State private var email: String = ""
+	@State private var password: String = ""
+	@State private var errorMessage: String = ""
+	@State private var confirmPassword: String = ""
+	
+	@State var changePage = false
+	@State var showTsAndCs = false
+	@State var agreeToTsAndCs = false
+	
+	var body: some View {
+		VStack {
+			Image("Building").resizable().aspectRatio(contentMode: .fit).padding()
+			Group {
+				Text("Sign Up").font(.largeTitle).bold().frame(maxWidth: .infinity, alignment: .leading)
+				TextField("Email", text: $email).textFieldStyle(RoundedBorderTextFieldStyle())
+				TextField("Password", text: $password).textFieldStyle(RoundedBorderTextFieldStyle())
+				TextField("Confirm password", text: self.$confirmPassword).textFieldStyle(RoundedBorderTextFieldStyle())
+				Toggle(isOn: $agreeToTsAndCs) {
+					Button(action: { showTsAndCs.toggle() }) {
+						Text("Accept Terms and Conditions").foregroundColor(Color.lightBackground).bold()
+					}
+				}.tint(.lightBackground).padding(.bottom)
+			}
+			StretchedButton(text: "SIGN UP", action: signUp)
+			Text(errorMessage).foregroundColor(Color.red).fixedSize(horizontal: false, vertical: true)
+			Spacer()
+			Button(action: { presentation.wrappedValue.dismiss() }) {
+				Text("Back to Login").foregroundColor(Color.lightBackground).bold()
+			}
+			NavigationLink(destination: CreateAccountView().environmentObject(session), isActive: self.$changePage) {
+				EmptyView()
+			}
+		}.padding().navigationBarHidden(true)
+			.sheet(isPresented: self.$showTsAndCs) {
+				TsAndCsView()
+			}
+	}
+	
+	func signUp() {
+		if !agreeToTsAndCs {
+			errorMessage = "Please accept the Terms and Conditions"
+		} else if email.isEmpty || password.isEmpty {
+			errorMessage = "Please provide an email and password"
+		} else if password == confirmPassword {
+			session.signUp(email: email, password: password) { (result, error) in
+				if let error = error {
+					errorMessage = error.localizedDescription
+				} else {
+					changePage = true
+				}
+			}
+		} else {
+			errorMessage = "Passwords do not match"
+		}
+	}
 }
---
+++ GetItWrite/Views/Settings/TsAndCsView.swift
@@ -0,0 +1,14 @@
+//
+//  TsAndCsView.swift
+//  GetItWrite
+//
+//  Created by Hannah Billingsley-Dadd on 01/07/2022.
+//
+
+import SwiftUI
+
+struct TsAndCsView: View {
+    var body: some View {
+        Text("Ts and Cs")
+    }
+}
