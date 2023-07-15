4c2e6ec5fd81f478f957c79fd30138daf08ac765
HannahDadd <hannahbdadd@googlemail.com>
2023-07-15 10:46:12 +0100

    ProposalsFeed

--- GetItWrite/Views/Proposals/ProposalsFeed.swift
+++ GetItWrite/Views/Proposals/ProposalsFeed.swift
@@ -15,22 +15,21 @@ struct ProposalsFeed: View {
     var body: some View {
         switch result {
         case .success(let proposals):
-            List {
-                StretchedButton(text: "Ask for Critiques", action: { self.showMakeProposalView.toggle() })
-                if proposals.count == 0 {
-                    Text("There are currently no requests for critiques.\n\nWhy not make one?")
-                } else {
-                    ForEach(proposals, id: \.id) { i in
-                        ProposalView(proposal: i).environmentObject(session)
+            VStack {
+                StretchedButton(text: "Ask for Critiques", action: { self.showMakeProposalView.toggle() }).padding()
+                List {
+                    if proposals.count == 0 {
+                        Text("There are currently no requests for critiques.\n\nWhy not make one?")
+                    } else {
+                        ForEach(proposals, id: \.id) { i in
+                            ProposalView(proposal: i).environmentObject(session)
+                        }
                     }
-                }
-            }.refreshable {
-                loadProposals()
-            }.listStyle(.plain).toolbar {
-                ToolbarItem(placement: .principal) {
-                    Image("Proposals").resizable().aspectRatio(contentMode: .fit)
-                }
-            }.sheet(isPresented: self.$showMakeProposalView) {
+                }.refreshable {
+                    loadProposals()
+                }.listStyle(.plain)
+            }
+            .sheet(isPresented: self.$showMakeProposalView) {
                 MakeProposalsView(showMakeProposalView: self.$showMakeProposalView).environmentObject(self.session)
             }
         case .failure(let error):
