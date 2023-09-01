5df38abc6821f7db0fd2cbeda9aac2158bbdee8c
HannahDadd <hannahbdadd@googlemail.com>
2023-09-01 16:23:34 +0100

    add button to message

--- GetItWrite/Views/Messages/MessagesView.swift
+++ GetItWrite/Views/Messages/MessagesView.swift
@@ -8,41 +8,41 @@
 import SwiftUI
 
 struct MessagesView: View {
-	@EnvironmentObject var session: FirebaseSession
-	@State private var result: Result<(String, [Message]), Error>?
-	@State var message: String = ""
-	@State var showMakeCritiqueView = false
-
-	let user2Id: String
-	let user2Username: String
-
-	var body: some View {
-		switch result {
-		case .success(let chatDetails):
-			ScrollViewReader { value in
-				VStack {
-					if chatDetails.1.count == 0 {
-						VStack {
-							Text("No messages yet").font(.caption)
-						}
-					} else {
-						ScrollView(.vertical, showsIndicators: false) {
-							ForEach(chatDetails.1, id: \.id) { m in
-								if m.senderID == session.userData?.id {
-									SelfChatBubble(message: m).id(chatDetails.1.firstIndex(of: m))
-								} else {
-									OtherUserChatBubble(message: m).id(chatDetails.1.firstIndex(of: m))
-								}
-							}.onChange(of: chatDetails.1.count) { count in
-								withAnimation {
-									value.scrollTo(count - 1)
-								}
-							}.onAppear {
-								value.scrollTo((chatDetails.1.count-1))
-							}
-						}
-					}
-					Spacer()
+    @EnvironmentObject var session: FirebaseSession
+    @State private var result: Result<(String, [Message]), Error>?
+    @State var message: String = ""
+    @State var showMakeCritiqueView = false
+    
+    let user2Id: String
+    let user2Username: String
+    
+    var body: some View {
+        switch result {
+        case .success(let chatDetails):
+            ScrollViewReader { value in
+                VStack {
+                    if chatDetails.1.count == 0 {
+                        VStack {
+                            Text("No messages yet").font(.caption)
+                        }
+                    } else {
+                        ScrollView(.vertical, showsIndicators: false) {
+                            ForEach(chatDetails.1, id: \.id) { m in
+                                if m.senderID == session.userData?.id {
+                                    SelfChatBubble(message: m).id(chatDetails.1.firstIndex(of: m))
+                                } else {
+                                    OtherUserChatBubble(message: m).id(chatDetails.1.firstIndex(of: m))
+                                }
+                            }.onChange(of: chatDetails.1.count) { count in
+                                withAnimation {
+                                    value.scrollTo(count - 1)
+                                }
+                            }.onAppear {
+                                value.scrollTo((chatDetails.1.count-1))
+                            }
+                        }
+                    }
+                    Spacer()
                     VStack {
                         StretchedButton(text: "Send work to \(user2Username)", action: { self.showMakeCritiqueView.toggle() })
                             .padding()
@@ -50,25 +50,22 @@ struct MessagesView: View {
                             session.sendMessage(content: message, chatId: chatDetails.0)
                         })
                     }
-				}
-			}.navigationBarTitle(Text(user2Username), displayMode: .inline)
-//                .navigationBarItems(
-//				trailing: Button(action: { self.showMakeCritiqueView.toggle() }) {
-//					Image(systemName: "pencil.tip.crop.circle.badge.arrow.forward")
-//				})
-				.sheet(isPresented: self.$showMakeCritiqueView) {
-					MakeTextView(chatId: chatDetails.0, userId: user2Id, displayName2: user2Username, showMakeCritiqueView: self.$showMakeCritiqueView).environmentObject(session)
-				}
-		case .failure(let error):
-			ErrorView(error: error, retryHandler: loadMessages)
-		case nil:
-			ProgressView().onAppear(perform: loadMessages)
-		}
-	}
-
-	private func loadMessages() {
-		session.loadChat(user2UID: user2Id) {
-			result = $0
-		}
-	}
+                }
+            }
+            .navigationBarTitle(Text(user2Username), displayMode: .inline)
+            .sheet(isPresented: self.$showMakeCritiqueView) {
+                MakeTextView(chatId: chatDetails.0, userId: user2Id, displayName2: user2Username, showMakeCritiqueView: self.$showMakeCritiqueView).environmentObject(session)
+            }
+        case .failure(let error):
+            ErrorView(error: error, retryHandler: loadMessages)
+        case nil:
+            ProgressView().onAppear(perform: loadMessages)
+        }
+    }
+    
+    private func loadMessages() {
+        session.loadChat(user2UID: user2Id) {
+            result = $0
+        }
+    }
 }
