68ff75f6c8f0190d4c0ac2f4dbca0dd5b5827e53
Hannah Dadd <hannahbdadd@googlemail.com>
2026-01-09 14:10:52 +0000

    colourful badges

--- GetItWrite/Views/Pages/HomepagePage.swift
+++ GetItWrite/Views/Pages/HomepagePage.swift
@@ -8,46 +8,13 @@
 import SwiftUI
 
 struct HomepagePage: View {
-    @StateObject private var navigationManager = NavigationManager()
+    @StateObject private var navigationManager = NavigationManager<HomepageRoute>()
     @State var path = NavigationPath([HomepageRoute.sprint])
     
     var body: some View {
         NavigationStack(path: $navigationManager.path) {
-            ScrollViewReader { value in
                 VStack {
                     HeadlineAndSubtitle(title: "Celebrate your Wins", subtitle: "Writing games to keep you on top form.")
-                    ScrollView(.horizontal) {
-                        HStack {
-                            Button("Sprint!") {
-                                value.scrollTo(0)
-                            }
-                            .padding()
-                            .foregroundColor(.onPrimary)
-                            .background(.primary)
-                            .clipShape(Capsule())
-                            Button("Writing Schedule") {
-                                value.scrollTo(1)
-                            }
-                            .padding()
-                            .foregroundColor(.onPrimary)
-                            .background(.primary)
-                            .clipShape(Capsule())
-                            Button("Your WIPs") {
-                                value.scrollTo(2)
-                            }
-                            .padding()
-                            .foregroundColor(.onPrimary)
-                            .background(.primary)
-                            .clipShape(Capsule())
-                            Button("Graphs") {
-                                value.scrollTo(3)
-                            }
-                            .padding()
-                            .foregroundColor(.onPrimary)
-                            .background(.primary)
-                            .clipShape(Capsule())
-                        }.padding()
-                    }
                     ScrollView {
                         VStack(spacing: 20) {
                             SprintCTA(action: {
@@ -57,16 +24,10 @@ struct HomepagePage: View {
                             Divider()
                             CommitmentCTA()
                                 .id(1)
-                            Divider()
-                            WIPsCTA()
-                                .id(2)
-                            GraphForWriter()
-                                .id(3)
                         }
                         .padding()
                     }
                     .scrollIndicators(.hidden)
-                }
             }
             .navigationDestination(for: HomepageRoute.self) { route in
                 switch route {
@@ -82,22 +43,4 @@ struct HomepagePage: View {
 
 enum HomepageRoute {
     case sprint
-}
-
-final class NavigationManager: ObservableObject {
-    @Published var path = NavigationPath()
-    
-    func navigate(to route: HomepageRoute) {
-        path.append(route)
-    }
-    
-    func pop() {
-        if !path.isEmpty {
-            path.removeLast()
-        }
-    }
-    
-    func reset() {
-        path = NavigationPath()
-    }
 }
