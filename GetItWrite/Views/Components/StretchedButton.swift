abdf2e14dcc909f7a3c9d094a0ef08af15ab9665
Hannah Dadd <hannahbdadd@googlemail.com>
2026-01-09 14:10:51 +0000

    remove firebase

---
+++ GetItWrite/Views/Components/StretchedButton.swift
@@ -0,0 +1,32 @@
+//
+//  StretchedButton.swift
+//  Get It Write
+//
+//  Created by Hannah Dadd on 15/08/2025.
+//
+
+import SwiftUI
+
+struct StretchedButton: View {
+    var text: String
+    var action: () -> Void
+    var size: CGFloat = 50
+    var isActive = true
+    
+    var body : some View {
+        Button(action: {
+            if isActive {
+                self.action()
+            }
+        }) {
+            Text(text)
+                .font(.headline)
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .foregroundColor(Color.onPrimary)
+                .padding()
+                .foregroundColor(Color.onPrimary)
+                .background(isActive ? Color.primary : .gray)
+                .overlay(RoundedRectangle(cornerRadius: 5))
+        }.accentColor(Color.clear)
+    }
+}
