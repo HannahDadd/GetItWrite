be1684dd6707b0c21113d644dbf58311ed4fb7c8
Hannah Dadd <hannahbdadd@googlemail.com>
2026-01-24 18:21:19 +0000

    some paw prints

--- GetItWrite/Views/Badges/BadgePage.swift
+++ GetItWrite/Views/Badges/BadgePage.swift
@@ -14,34 +14,14 @@ struct BadgePage: View {
             HeadlineAndSubtitle(title: "Your Progress", subtitle: "Writing games to keep you on top form.")
             Text("Your goal:")
                 .font(Font.custom("Bellefair-Regular", size: 24))
-            
-            VStack(spacing: 16) {
-                Image(systemName: "star.fill")
-                    .resizable()
-                    .frame(width: 40, height: 40)
-                
-                SDiagonalPath()
-                    .stroke(
-                        Color.black,
-                        style: StrokeStyle(
-                            lineWidth: 3,
-                            lineCap: .round,
-                            dash: [5, 8]
-                        )
-                    )
-                    .frame(height: 200)
-                    .padding()
-                
-                Image(systemName: "heart.fill")
-                    .resizable()
-                    .frame(width: 40, height: 40)
+            ScrollView {
+                VStack(spacing: 24) {
+                    ForEach(0..<25) {_ in
+                        Image(systemName: "pawprint.fill")
+                            .font(.system(size: 45))
+                    }
+                }
             }
-            .padding()
-            //            ScrollView {
-            //                VStack {
-            //
-            //                }
-            //            }
         }
         .padding()
     }
