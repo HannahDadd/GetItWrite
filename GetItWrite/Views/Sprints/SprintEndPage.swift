5003e7b02522ee2824af093e38b80ddb7ad2c3c8
Hannah Dadd <hannahbdadd@googlemail.com>
2026-02-15 08:43:14 +0000

    badges after sprint words

--- GetItWrite/Views/Sprints/SprintEndPage.swift
+++ GetItWrite/Views/Sprints/SprintEndPage.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct SprintEndPage: View {
     @AppStorage(UserDefaultNames.tally.rawValue) private var tally = 0
+    @AppStorage(UserDefaultNames.streakEnd.rawValue) private var streakEnd = 0
     
     @AppStorage(UserDefaultNames.quickWords250.rawValue) private var quickWords250 = false
     @AppStorage(UserDefaultNames.quickWords500.rawValue) private var quickWords500 = false
@@ -21,6 +22,18 @@ struct SprintEndPage: View {
     @AppStorage(UserDefaultNames.streakFreak14.rawValue) private var streakFreak14 = false
     @AppStorage(UserDefaultNames.streakFreak31.rawValue) private var streakFreak31 = false
     @AppStorage(UserDefaultNames.streakFreak100.rawValue) private var streakFreak100 = false
+    
+    @AppStorage(UserDefaultNames.wordNerd200.rawValue) private var wordNerd200 = false
+    @AppStorage(UserDefaultNames.wordNerd500.rawValue) private var wordNerd500 = false
+    @AppStorage(UserDefaultNames.wordNerd1000.rawValue) private var wordNerd1000 = false
+    @AppStorage(UserDefaultNames.wordNerd10000.rawValue) private var wordNerd10000 = false
+    @AppStorage(UserDefaultNames.wordNerd20000.rawValue) private var wordNerd20000 = false
+    @AppStorage(UserDefaultNames.wordNerd50000.rawValue) private var wordNerd50000 = false
+    
+    @AppStorage(UserDefaultNames.bookGoal.rawValue) private var bookGoal = false
+    @AppStorage(UserDefaultNames.hourSprint.rawValue) private var hourSprint = false
+    @AppStorage(UserDefaultNames.fortySprint.rawValue) private var fortySprint = false
+    @AppStorage(UserDefaultNames.twentySprint.rawValue) private var twentySprint = false
     
     @State var endWordCount: Int = 0
     @Binding var project: WIP?
@@ -113,6 +126,31 @@ struct SprintEndPage: View {
                 }
                 if changeWordCount > 99 {
                     streakFreak100 = true
+                }
+                
+                // Word badges
+                if tally > 199 {
+                    wordNerd200 = true
+                }
+                if tally > 499 {
+                    wordNerd500 = true
+                }
+                if tally > 999 {
+                    wordNerd1000 = true
+                }
+                if tally > 9999 {
+                    wordNerd10000 = true
+                }
+                if tally > 19999 {
+                    wordNerd20000 = true
+                }
+                if tally > 49999 {
+                    wordNerd50000 = true
+                }
+                
+                // Book goal
+                if tally >= 49999 {
+                    bookGoal = true
                 }
                 
                 sprintState = .end
