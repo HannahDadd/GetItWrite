a00bc87789dec45f9c1370cb576dbd9145613494
Hannah Dadd <hannahbdadd@googlemail.com>
2026-02-16 08:55:40 +0000

    badge style

--- GetItWrite/Views/Badges/BadgePage.swift
+++ GetItWrite/Views/Badges/BadgePage.swift
@@ -10,51 +10,76 @@ import SwiftUI
 struct BadgePage: View {
     
     var body: some View {
-        NavigationView {
-            List {
-                Section(header: Text("speed run").textCase(.uppercase)) {
-                    BadgePromo(badge: Badge.twentySprint)
+        GeometryReader { geo in
+            NavigationView {
+                List {
+                    HStack {
+                        VStack {
+                            Text(Badge.twentySprint.getText())
+                        }
+                        .frame(width: geo.size.width * 0.3, height: 150)
+                        .background(.white)
+                        .cornerRadius(8)
+                        Spacer()
+                        VStack {
+                            Text(Badge.twentySprint.getText())
+                        }
+                        .frame(width: geo.size.width * 0.3, height: 150)
+                        .background(.white)
+                        .cornerRadius(8)
+                        Spacer()
+                        VStack {
+                            Text(Badge.twentySprint.getText())
+                        }
+                        .frame(width: geo.size.width * 0.3, height: 150)
+                        .background(.white)
+                        .cornerRadius(8)
+                    }
+                    .listRowBackground(Color.clear)
+                    Section(header: Text("speed run").textCase(.uppercase)) {
+                        BadgePromo(badge: Badge.twentySprint)
+                    }
+                    
+                    Section(header: Text("Somewhere in the middle").textCase(.uppercase)) {
+                        BadgePromo(badge: Badge.fortySprint)
+                    }
+                    
+                    Section(header: Text("In for the long haul").textCase(.uppercase)) {
+                        BadgePromo(badge: Badge.hourSprint)
+                    }
+                    
+                    Section(header: Text("Word nerd").textCase(.uppercase)) {
+                        BadgePromo(badge: Badge.wordNerd200)
+                        BadgePromo(badge: Badge.wordNerd500)
+                        BadgePromo(badge: Badge.wordNerd1000)
+                        BadgePromo(badge: Badge.wordNerd10000)
+                        BadgePromo(badge: Badge.wordNerd20000)
+                        BadgePromo(badge: Badge.wordNerd50000)
+                    }
+                    
+                    Section(header: Text("streak freak").textCase(.uppercase)) {
+                        BadgePromo(badge: Badge.streakFreak2)
+                        BadgePromo(badge: Badge.streakFreak7)
+                        BadgePromo(badge: Badge.streakFreak14)
+                        BadgePromo(badge: Badge.streakFreak31)
+                        BadgePromo(badge: Badge.streakFreak100)
+                    }
+                    
+                    Section(header: Text("quick words").textCase(.uppercase)) {
+                        BadgePromo(badge: Badge.quickWords250)
+                        BadgePromo(badge: Badge.quickWords500)
+                        BadgePromo(badge: Badge.quickWords1000)
+                        BadgePromo(badge: Badge.quickWords2000)
+                        BadgePromo(badge: Badge.quickWords5000)
+                    }
+                    
+                    Section(header: Text("write a book").textCase(.uppercase)) {
+                        BadgePromo(badge: Badge.bookGoal)
+                    }
                 }
-                
-                Section(header: Text("Somewhere in the middle").textCase(.uppercase)) {
-                    BadgePromo(badge: Badge.fortySprint)
-                }
-                
-                Section(header: Text("In for the long haul").textCase(.uppercase)) {
-                    BadgePromo(badge: Badge.hourSprint)
-                }
-                
-                Section(header: Text("Word nerd").textCase(.uppercase)) {
-                    BadgePromo(badge: Badge.wordNerd200)
-                    BadgePromo(badge: Badge.wordNerd500)
-                    BadgePromo(badge: Badge.wordNerd1000)
-                    BadgePromo(badge: Badge.wordNerd10000)
-                    BadgePromo(badge: Badge.wordNerd20000)
-                    BadgePromo(badge: Badge.wordNerd50000)
-                }
-                
-                Section(header: Text("streak freak").textCase(.uppercase)) {
-                    BadgePromo(badge: Badge.streakFreak2)
-                    BadgePromo(badge: Badge.streakFreak7)
-                    BadgePromo(badge: Badge.streakFreak14)
-                    BadgePromo(badge: Badge.streakFreak31)
-                    BadgePromo(badge: Badge.streakFreak100)
-                }
-                
-                Section(header: Text("quick words").textCase(.uppercase)) {
-                    BadgePromo(badge: Badge.quickWords250)
-                    BadgePromo(badge: Badge.quickWords500)
-                    BadgePromo(badge: Badge.quickWords1000)
-                    BadgePromo(badge: Badge.quickWords2000)
-                    BadgePromo(badge: Badge.quickWords5000)
-                }
-                
-                Section(header: Text("write a book").textCase(.uppercase)) {
-                    BadgePromo(badge: Badge.bookGoal)
-                }
+                .listStyle(.insetGrouped)
+                .navigationBarTitle("Achievements")
             }
-            .listStyle(.insetGrouped)
-            .navigationBarTitle("Achievements")
         }
     }
 }
