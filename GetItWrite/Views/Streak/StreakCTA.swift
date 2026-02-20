9cc7eb4983f4689e4029dcd55434cb872a140d98
Hannah Dadd <hannahbdadd@googlemail.com>
2026-02-20 20:34:24 +0000

    isdaylit

--- GetItWrite/Views/Streak/StreakCTA.swift
+++ GetItWrite/Views/Streak/StreakCTA.swift
@@ -6,16 +6,17 @@
 //
 
 import SwiftUI
+import GetItWriteShared
 
 struct StreakCTA: View {
-    @AppStorage(UserDefaultNames.streakStart.rawValue) private var streakStart: Double = 0
-    @AppStorage(UserDefaultNames.streakEnd.rawValue) private var streakEnd: Double = 0
+    @AppStorage(UserDefaultNames.streakCount.rawValue) private var streakCount = 0
+    @AppStorage(UserDefaultNames.lastActivityDate.rawValue) private var lastActivityTimestamp: Double = 0
     
     var body: some View {
         VStack(spacing: 24) {
             HStack {
                 Spacer()
-                Text("\(StreakCTA.getNumberOfDays(streakStart: streakStart, streakEnd: streakEnd))")
+                Text("\(streakCount)")
                     .font(Font.custom("AbrilFatface-Regular", size: 54))
                     .padding()
                 Spacer()
@@ -24,44 +25,14 @@ struct StreakCTA: View {
                 Spacer()
             }
             HStack {
-                StreakDay(dayInitial: "M", dayComplete: isDayLit(offset: 0))
-                StreakDay(dayInitial: "T", dayComplete: isDayLit(offset: 1))
-                StreakDay(dayInitial: "W", dayComplete: isDayLit(offset: 2))
-                StreakDay(dayInitial: "T", dayComplete: isDayLit(offset: 3))
-                StreakDay(dayInitial: "F", dayComplete: isDayLit(offset: 4))
-                StreakDay(dayInitial: "S", dayComplete: isDayLit(offset: 5))
-                StreakDay(dayInitial: "S", dayComplete: isDayLit(offset: 6))
+                StreakDay(dayInitial: "M", dayComplete: GetItWriteShared.isDayLit(offset: 0, streakCount: streakCount, lastActivityTimestamp: lastActivityTimestamp))
+                StreakDay(dayInitial: "T", dayComplete: GetItWriteShared.isDayLit(offset: 1, streakCount: streakCount, lastActivityTimestamp: lastActivityTimestamp))
+                StreakDay(dayInitial: "W", dayComplete: GetItWriteShared.isDayLit(offset: 2, streakCount: streakCount, lastActivityTimestamp: lastActivityTimestamp))
+                StreakDay(dayInitial: "T", dayComplete: GetItWriteShared.isDayLit(offset: 3, streakCount: streakCount, lastActivityTimestamp: lastActivityTimestamp))
+                StreakDay(dayInitial: "F", dayComplete: GetItWriteShared.isDayLit(offset: 4, streakCount: streakCount, lastActivityTimestamp: lastActivityTimestamp))
+                StreakDay(dayInitial: "S", dayComplete: GetItWriteShared.isDayLit(offset: 5, streakCount: streakCount, lastActivityTimestamp: lastActivityTimestamp))
+                StreakDay(dayInitial: "S", dayComplete: GetItWriteShared.isDayLit(offset: 6, streakCount: streakCount, lastActivityTimestamp: lastActivityTimestamp))
             }
         }
-    }
-    
-    static func getNumberOfDays(streakStart: Double, streakEnd: Double) -> Int {
-        let startDate = Date.init(timeIntervalSince1970: streakStart)
-        let endDate = Date.init(timeIntervalSince1970: streakEnd)
-        
-        let betweenEndAndNow = Calendar.current.dateComponents([.day], from: endDate, to: Date.now).day ?? 0
-        if betweenEndAndNow >= 1 {
-            return 0
-        }
-        
-        let streak = Calendar.current.dateComponents([.day], from: startDate, to: endDate).day ?? 0
-        if streak == 0 && streakEnd > streakStart {
-            return 1
-        }
-        return streak
-    }
-    
-    func isDayLit(offset: Int) -> Bool {
-        let calendar = Calendar.current
-        let endDate = calendar.startOfDay(for: Date.init(timeIntervalSince1970: streakEnd))
-        let today = calendar.startOfDay(for: Date())
-        let startDate = calendar.startOfDay(for: Date.init(timeIntervalSince1970: streakStart + 86000))
-        let weekStart = calendar.dateInterval(of: .weekOfYear, for: today)!.start
-        
-        let day = calendar.date(byAdding: .day, value: offset, to: weekStart)!
-        
-        let isCompleted = day >= startDate && day <= endDate
-        
-        return isCompleted
     }
 }
