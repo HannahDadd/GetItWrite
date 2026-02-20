c17f47ddd67ca09231f7d7c1cd35f717e87a43cb
Hannah Dadd <hannahbdadd@googlemail.com>
2026-02-20 20:44:20 +0000

    live activity

---
+++ SprintLiveActivity/SprintLiveActivityBundle.swift
@@ -0,0 +1,18 @@
+//
+//  SprintLiveActivityBundle.swift
+//  SprintLiveActivity
+//
+//  Created by Hannah Dadd on 20/02/2026.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SprintLiveActivityBundle: WidgetBundle {
+    var body: some Widget {
+        SprintLiveActivity()
+        SprintLiveActivityControl()
+        SprintLiveActivityLiveActivity()
+    }
+}
